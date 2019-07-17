91e853dff8ce8651334bfd1d47f15646ee1bd1c7
Chris <burke1791@gmail.com>
2019-07-17 18:08:38 -0500

    installed axios as a dependency and made a call to the scrape route on App.js mount

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  useEffect(() => {
+    axios.get('/api/scrape').then(response => {
+      console.log(response);
+    });
+  });
+  
   return (
     <div className="App">
       IB Scraper
