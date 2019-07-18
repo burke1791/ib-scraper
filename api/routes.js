ddbb92466f561b757fc4508fee597d00c1f44442
Chris Burke <burkcules@Chriss-iMac.local>
2019-07-18 01:56:41 -0500

    completed scraper route and added a helper function to insert new record into db only if it doesn't already exist

--- api/routes.js
+++ api/routes.js
@@ -2,21 +2,38 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const db = require('../models');
 
-module.exports = function(app) {
+module.exports = function (app) {
   app.get('/api/scrape', (req, res, next) => {
     console.log('get articles hit');
-    axios.get('https://illiniboard.com/').then(response => {
+    let url = 'https://illiniboard.com';
+    axios.get(url).then(response => {
       var $ = cheerio.load(response.data);
 
-      $('.hp_secondary_article h4').each((i, element) => {
+      $('.hp_secondary_article').each(function (i, element) {
         var result = {};
 
-        result.title = $(this).children('a').text();
+        result.title = $(this).find('h4 a').text();
+        result.link = url + $(this).find('h4 a').attr('href');
+        result.summary = $(this).children('p').text();
 
-        console.log(result);
+        // console.log(result);
+        insertIfNotDuplicate(result);
       });
 
-      res.send(response.data);
+      $('.hp_article_list_element').each(function (i, element) {
+        var result = {};
+
+        result.title = $(this).find('h4').text();
+        result.link = url + $(this).find('h4 a').attr('href');
+        result.summary = $(this).find('p').text();
+
+        // console.log(result);
+        insertIfNotDuplicate(result);
+      })
+
+      res.status(200).json({
+        message: 'scrape complete'
+      });
     });
   });
 
@@ -30,5 +47,21 @@ module.exports = function(app) {
 
   app.post('/api/articles/:id', (req, res, next) => {
     // post the user's note to the article by its id
+  });
+}
+
+const insertIfNotDuplicate = (article) => {
+  db.Article.find({ title: article.title }).then(dbArticle => {
+    console.log(dbArticle);
+    if (dbArticle.length) {
+      console.log('supposedly already exists - don\'t duplicate');
+    } else {
+      console.log('article doesn\'t already exists - insert into db');
+      db.Article.create(article).then(insertedArticle => {
+        console.log(insertedArticle);
+      });
+    }
+  }).catch(error => {
+    console.log(error);
   });
 }
