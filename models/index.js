afb077779b5db12c3d7fab8138aafe583f6d6bd4
Chris Burke <burkcules@Chriss-iMac.local>
2019-07-18 01:57:08 -0500

    required connection.js so that the server actually connects to mongo

--- models/index.js
+++ models/index.js
@@ -1,3 +1,5 @@
+const connection = require('../connection');
+
 // exports all models from one file so the api only needs to import from "./models"
 module.exports = {
   Article: require('./article'),
